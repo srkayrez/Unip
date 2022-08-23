ad4ea20b5398efa86a4899ff2855a0b9237a3f68
João Victor Caires <srkayrez@gmail.com>
2022-08-23 14:31:43 -0300

    ta funfando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
-interface emails {
-  value: string;
-  viewValue: string;
+export interface HorarioAula {
+  Segunda: string;
+  Terca: string;
+  Quarta: string;
+  QuintaPar: string;
+  QuintaImp: string;
+  Sexta: string;
 }
+
+const ELEMENT_DATA: HorarioAula[] = [
+  { Segunda: 'Qualidade de Software',
+    Terca: 'Des. de Sistemas Distribuidos',
+    Quarta: 'Qualidade de Software',
+    QuintaPar: 'Trabalho de Curso II',
+    QuintaImp: 'Orientação - Estágio',
+    Sexta: '-' 
+  },
+  { Segunda: 'Qualidade de Software',
+    Terca: 'Des. de Sistemas Distribuidos',
+    Quarta: 'Qualidade de Software',
+    QuintaPar: 'Trabalho de Curso II',
+    QuintaImp: 'Orientação - Estágio',
+    Sexta: '-' 
+  },
+]
+  
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -12,6 +36,8 @@ interface emails {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  displayedColumns: string[] = ['Segunda', 'Terça', 'Quarta', 'QuintaPar', 'QuintaImp', 'Sexta'];
+  dataSource = ELEMENT_DATA;
   title = 'Unip';
   panelOpenState = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import {MatDividerModule} from '@angular/material/divider';
     MatSelectModule,
     MatExpansionModule,
     MatDividerModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
